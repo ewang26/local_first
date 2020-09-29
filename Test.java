fe0db387f94eda065f19b6261ca9ac46754251e8
Erik Wang <ewang21@andover.edu>
2020-09-29 18:36:31 -0400

    Added stuff

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hi");
+        System.out.println(1);
+        int x = 0;
+        int y = 8;
+        System.out.println(x+y);
+        int z = 1;
+    }
+}
